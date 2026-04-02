765cf3839b2c163841f328018de94aecae7370a3
taekseong <retake_joy@hanpass.com>
2026-04-02 17:55:37 +0900

    test: 헤더 상태 전이 Swift Testing 추가

--- Feature/CarveFeature/Tests/HeaderFeatureTesting.swift
+++ Feature/CarveFeature/Tests/HeaderFeatureTesting.swift
@@ -9,6 +9,25 @@
 import Testing
 
 struct HeaderFeatureTesting {
+    @Test("헤더를 다시 토글해 보이면 오프셋을 0으로 되돌리고 추적 상태를 초기화한다")
+    func toggleVisibilityShowsHeaderAndResetsTracking() async {
+        var state = HeaderFeature.State.initialState
+        state.headerHeight = 72
+        state.headerOffset = -72
+        state.lastHeaderOffset = -72
+        state.direction = .down
+        state.shiftOffset = 18
+        state.isHidden = true
+
+        _ = HeaderFeature().reduce(into: &state, action: .toggleVisibility)
+
+        #expect(!state.isHidden)
+        #expect(state.headerOffset == 0)
+        #expect(state.direction == .none)
+        #expect(state.shiftOffset == 0)
+        #expect(state.lastHeaderOffset == 0)
+    }
+
     @Test("헤더 토글로 숨길 때 높이만큼 올리고 추적 상태를 초기화한다")
     func toggleVisibilityHidesHeaderAndResetsTracking() async {
         var state = HeaderFeature.State.initialState
@@ -39,6 +58,22 @@ struct HeaderFeatureTesting {
         #expect(state.headerOffset == -72)
     }
 
+    @Test("아래로 스크롤하면 헤더 오프셋은 0을 넘지 않고 현재 위치를 기준으로 전환한다")
+    func headerAnimationClampsOffsetAtZeroWhileScrollingDown() async {
+        var state = HeaderFeature.State.initialState
+        state.headerHeight = 72
+        state.headerOffset = -24
+        state.direction = .up
+        state.shiftOffset = -12
+
+        _ = HeaderFeature().reduce(into: &state, action: .headerAnimation(-12, 32))
+
+        #expect(state.direction == .down)
+        #expect(state.shiftOffset == 32)
+        #expect(state.lastHeaderOffset == -24)
+        #expect(state.headerOffset == -24)
+    }
+
     @Test("탭으로 숨긴 뒤 아래로 스크롤하면 숨김 상태를 해제하고 아래 방향으로 전환한다")
     func headerAnimationClearsHiddenStateOnScrollDown() async {
         var state = HeaderFeature.State.initialState
@@ -52,5 +87,14 @@ struct HeaderFeatureTesting {
         #expect(state.direction == .down)
         #expect(state.shiftOffset == 20)
         #expect(state.lastHeaderOffset == -72)
+    }
+
+    @Test("헤더 높이 설정 액션은 측정한 높이를 상태에 반영한다")
+    func setHeaderHeightUpdatesState() async {
+        var state = HeaderFeature.State.initialState
+
+        _ = HeaderFeature().reduce(into: &state, action: .view(.setHeaderHeight(88)))
+
+        #expect(state.headerHeight == 88)
     }
 }
